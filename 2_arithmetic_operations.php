283fcb479f812f7f14d8c9d0cac6e831d4cb5267
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2025-01-19 19:44:04 +0530

    Add arithmetic-operations.php: Program to perform basic arithmetic operations.

---
+++ 2_arithmetic_operations.php
@@ -0,0 +1,8 @@
+<?php
+$a = 10;
+$b = 5;
+echo "Addition: " . ($a + $b) . "\n";
+echo "Subtraction: " . ($a - $b) . "\n";
+echo "Multiplication: " . ($a * $b) . "\n";
+echo "Division: " . ($a / $b) . "\n";
+?>
