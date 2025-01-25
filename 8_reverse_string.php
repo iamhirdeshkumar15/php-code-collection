8bbfff24c6926b2a45df672b055b54ab6907932f
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2025-01-25 16:30:54 +0530

    Program to reverse a given string using strrev().

---
+++ 8_reverse_string.php
@@ -0,0 +1,8 @@
+# Program to reverse a given string using strrev()
+
+<?php
+$string = "Hello";
+$reversed = strrev($string);
+echo "Original String: $string \n";
+echo "Reversed String: $reversed";
+?>
