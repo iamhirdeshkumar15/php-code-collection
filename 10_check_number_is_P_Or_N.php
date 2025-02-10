101ba2e9ddb8c3b6e064adc2ab44b5c28a16015c
Hirdesh Kumar Yadav <hirdeshk976@gmail.com>
2025-02-10 14:48:46 +0530

    Add prime-check.php: Check if a number is prime.

--- 10_check_number_is_P_Or_N.php
+++ 10_check_number_is_P_Or_N.php
@@ -9,3 +9,16 @@ if ($num > 0){
     echo "$num is zero.";
 }
 ?>
+
+
+// another 
+<?php
+function isPrime($num) {
+    if ($num < 2) return false;
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i == 0) return false;
+    }
+    return true;
+}
+echo isPrime(7) ? "Prime" : "Not Prime";
+?>
